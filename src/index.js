99833f01a0cf06975cf611b050b91877540bd429
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-04-23 15:37:55 +0300

    chore(defaults): update (#19)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import loaderUtils from 'loader-utils';
+import validateOptions from 'schema-utils';
+
+import schema from './options.json';
+
 export default function() {
   return '// empty (null-loader)';
 }
 
 export function pitch() {
+  const options = loaderUtils.getOptions(this) || {};
+
+  validateOptions(schema, options, 'Null Loader');
+
   return '// empty (null-loader)';
 }
--- test/null-loader.test.js
+++
@@ -1,9 +0,0 @@
-import { name as PROJECT_NAME } from '../package.json';
-import NullLoader from '../src';
-
-describe(PROJECT_NAME, () => {
-  test('should export the loader', (done) => {
-    expect(NullLoader).toBeInstanceOf(Function);
-    done();
-  });
-});
