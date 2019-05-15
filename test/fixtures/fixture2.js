71e7c0e990e10cba79274bc0d722bf452f0db701
evilebottnawi <sheo13666q@gmail.com>
2019-05-15 14:17:00 +0300

    fix: handle not javascript modules
    BREAKING CHANGE: loader don't generate comment by default

---
+++ test/fixtures/fixture2.js
@@ -0,0 +1,3 @@
+import styles from './fixture.css';
+
+export default styles;
--- test/loader.test.js
+++ test/loader.test.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import webpack from './helpers/compiler';
 
 describe('null-loader', () => {
@@ -18,13 +20,39 @@ describe('null-loader', () => {
     expect(stats.compilation.assets['main.js'].source()).toMatchSnapshot();
   });
 
-  it.only('should works as inline loader', async () => {
+  it('should works as inline loader', async () => {
     const config = {
       pathinfo: false,
       devtool: false,
     };
 
     const stats = await webpack('inline-loader.js', config);
+    const { warnings, errors } = stats.toJson();
+
+    expect(warnings).toMatchSnapshot('warnings');
+    expect(errors).toMatchSnapshot('errors');
+
+    expect(stats.compilation.assets['main.js'].source()).toMatchSnapshot();
+  });
+
+  it('should works with not js modules', async () => {
+    const config = {
+      pathinfo: false,
+      devtool: false,
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'css-loader',
+            {
+              loader: path.resolve(__dirname, '../src'),
+            },
+          ],
+        },
+      ],
+    };
+
+    const stats = await webpack('fixture2.js', config);
     const { warnings, errors } = stats.toJson();
 
     expect(warnings).toMatchSnapshot('warnings');
