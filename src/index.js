bd6c18dd8564fa8da41b463f06c7e178fb569218
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-04-15 15:52:05 +0300

    chore(deps): update
    BREAKING CHANGE: minimum required Nodejs version is `10.13`

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import validateOptions from 'schema-utils';
 
 import schema from './options.json';
 
-export default function() {
+export default function loader() {
   return '';
 }
 
 export function pitch() {
-  const options = loaderUtils.getOptions(this) || {};
+  const options = loaderUtils.getOptions(this);
 
-  validateOptions(schema, options, 'Null Loader');
+  validateOptions(schema, options, {
+    name: 'Null Loader',
+    baseDataPath: 'options',
+  });
 
   return '';
 }
--- test/loader.test.js
+++ test/loader.test.js
@@ -1,63 +1,46 @@
-import path from 'path';
-
-import webpack from './helpers/compiler';
+import {
+  getCompiler,
+  compile,
+  getModuleSource,
+  getExecutedCode,
+} from './helpers/index';
 
 describe('null-loader', () => {
   it('should works', async () => {
-    const config = {
-      devtool: false,
-      loader: {
-        test: /\.js$/,
-      },
-    };
+    const compiler = getCompiler('./fixture.js');
+    const stats = await compile(compiler);
 
-    const stats = await webpack('fixture.js', config);
-    const { warnings, errors } = stats.toJson();
-
-    expect(warnings).toMatchSnapshot('warnings');
-    expect(errors).toMatchSnapshot('errors');
-
-    expect(stats.compilation.assets['main.js'].source()).toMatchSnapshot();
+    expect(getModuleSource('./fixture.js', stats)).toMatchSnapshot('module');
+    expect(getExecutedCode('main.bundle.js', compiler, stats)).toMatchSnapshot(
+      'result'
+    );
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
 
   it('should works as inline loader', async () => {
-    const config = {
-      pathinfo: false,
-      devtool: false,
-    };
+    const compiler = getCompiler('./inline-loader.js');
+    const stats = await compile(compiler);
 
-    const stats = await webpack('inline-loader.js', config);
-    const { warnings, errors } = stats.toJson();
-
-    expect(warnings).toMatchSnapshot('warnings');
-    expect(errors).toMatchSnapshot('errors');
-
-    expect(stats.compilation.assets['main.js'].source()).toMatchSnapshot();
+    expect(getModuleSource('./inline-loader.js', stats)).toMatchSnapshot(
+      'module'
+    );
+    expect(getExecutedCode('main.bundle.js', compiler, stats)).toMatchSnapshot(
+      'result'
+    );
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
 
   it('should works with not js modules', async () => {
-    const config = {
-      pathinfo: false,
-      devtool: false,
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'css-loader',
-            {
-              loader: path.resolve(__dirname, '../src'),
-            },
-          ],
-        },
-      ],
-    };
+    const compiler = getCompiler('./fixture2.js');
+    const stats = await compile(compiler);
 
-    const stats = await webpack('fixture2.js', config);
-    const { warnings, errors } = stats.toJson();
-
-    expect(warnings).toMatchSnapshot('warnings');
-    expect(errors).toMatchSnapshot('errors');
-
-    expect(stats.compilation.assets['main.js'].source()).toMatchSnapshot();
+    expect(getModuleSource('./fixture2.js', stats)).toMatchSnapshot('module');
+    expect(getExecutedCode('main.bundle.js', compiler, stats)).toMatchSnapshot(
+      'result'
+    );
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
 });
